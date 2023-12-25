7dbc12c90b3558f6ba25c210850cdcba341d1907
Azim <azimarynaliev@icloud.com>
2023-12-25 16:27:41 -0600

    updated

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const fetchDeckCard = async () => {
         "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1"
       );
       const data = await res.json();
-      localStorage.setItem("deckId", data.deck_id);
+      localStorage.setItem("deck_id", data.deck_id);
       return data.deck_id;
     } else {
       return localDeckId;
@@ -25,16 +25,19 @@ const drawCards = async () => {
       `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=6`
     );
     const data = await res.json();
-    let cards = data.card;
-    cards.forEach((el) => {
+    let cards = data.cards;
+    const cardsImg = cards.forEach((el) => {
       const img = document.createElement("img");
       img.src = el.image;
       img.style.width = "40px";
-      const container = document.getElementsByClassName("container");
-      container.appendChild(img);
+      const player1 = document.querySelector(".player1");
+      const player2 = document.querySelector(".player2");
+
+      // player1.append(img);
+      // player2.append(img);
     });
 
-    //  console.log(data);
+    console.log(data);
     return data;
   } catch (err) {
     console.log(err);
