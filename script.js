18730fc04d52270568a95ebc43b95b467d4a0574
Azim <azimarynaliev@icloud.com>
2023-12-25 20:09:46 -0600

    updated

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 "use strict";
 
+let player1Cards = [];
+let player3Cards = [];
+let cardsOnTable = [];
+let prevCard = {};
+let lastCard = {};
+let handsOnTable = [];
+
 const fetchDeckCard = async () => {
   try {
     const localDeckId = localStorage.getItem("deck_id");
@@ -18,24 +25,19 @@ const fetchDeckCard = async () => {
   }
 };
 
-let numOfPlayers;
-const onChangeHandler = (e) => {
-  numOfPlayers = e.target.value;
-  return numOfPlayers;
-};
-
 const drawCards = async () => {
   try {
     let deck_id = await fetchDeckCard();
     const res = await fetch(
-      `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=52`
+      `https://www.deckofcardsapi.com/api/deck/${deck_id}/draw/?count=20`
     );
     const data = await res.json();
 
     let cards = data.cards;
     const cardsImg = cards.forEach((el, index) => {
       const img = document.createElement("img");
-      img.src = el.image;
+      img.src =
+        "https://images.squarespace-cdn.com/content/v1/5abd8db4620b85fa99f15131/1542340370129-WV43BVUJLUTWL6FRRK52/Card+Back+2.0+-+Poker+Size+-+Red_shw.png";
       img.style.width = "100px";
 
       const player1 = document.querySelector(".player1");
@@ -57,5 +59,3 @@ const drawCards = async () => {
     console.log("Error drawing cards", err);
   }
 };
-
-drawCards();
